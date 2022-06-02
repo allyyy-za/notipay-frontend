5604b7bef69ee54a489c4378a547439b3dd9a545
Allyza Maureen Catura <amaureencatura@gmail.com>
2022-06-02 22:03:34 +0800

    Added BillView

--- src/pages/Home/components/modals/BillView.jsx
+++ src/pages/Home/components/modals/BillView.jsx
@@ -1,98 +1,126 @@
-import { Grid, Typography } from '@mui/material';
-import React, { useState } from 'react'
-import { Navigate, useNavigate } from 'react-router-dom';
-import CustomButton from '../../../../components/controls/CustomButton';
-import CustomTextField from '../../../../components/controls/CustomTextField';
-import Popup from '../../../../components/controls/Popup';
-import { useLocalState } from '../../../../util/useLocalStorage'
-import styles from './styles/BillView.module.css';
+import { Grid, Typography } from "@mui/material";
+import React, { Component } from "react";
+import CustomButton from "../../../../components/controls/CustomButton";
+import CustomTextField from "../../../../components/controls/CustomTextField";
+import Popup from "../../../../components/controls/Popup";
+import styles from "./styles/BillView.module.css";
 
-export default function BillView(props) {
-  const [auth, setAuth] = useLocalState("", "authToken")
-  const [billName, setName] = useState("");
-  const [amount, setAmount] = useState("");
-  const [dueDate, setDate] = useState(props.billView.dueDate);
-  const [hasValue, setHasValue] = useState(false);
-  const [focus, setFocused] = useState(false);
+export default class BillView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      billId: 0,
+      billName: "",
+      amount: 0,
+      dueDate: "",
+      focus: false,
+      hasValue: false,
+    };
+  }
 
-  const onFocus = () => setFocused(true);
-  const onBlur = () => setFocused(false);
-  const navigate = useNavigate();
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      billId: nextProps.billView.billId,
+      billName: nextProps.billView.billName,
+      amount: nextProps.billView.amount,
+      dueDate: nextProps.billView.dueDate,
+      focus: false,
+      hasValue: false,
+    });
+  }
 
-  const handleEditBill = (e) => {
+  billNameHandler(e) {
+    this.setState({ billName: e.target.value });
+  }
+
+  amountHandler(e) {
+    this.setState({ amount: e.target.value });
+  }
+
+  dueDateHandler(e) {
+    this.setState({ dueDate: e.target.value });
+  }
+
+  onFocus = () => this.setState({ focus: true });
+  onBlur = () => this.setState({ focus: false });
+  setHasValue = () => this.setState({ hasValue: true });
+
+  handleEditBill = (e) => {
     e.preventDefault();
-    const bill = { billName, amount, dueDate };
-    fetch("api/bills/billId=" + props.billView.billId, {
+    const bill = this.state;
+    const auth = localStorage.getItem("authToken");
+    fetch("api/bills/billId=" + this.state.billId, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${auth}`,
-      }, body: JSON.stringify(bill),
+        Authorization: `Bearer ${JSON.parse(auth)}`,
+      },
+      body: JSON.stringify(bill),
     }).then((response) => {
       if (response.status === 200) {
         console.log("Bill edited.");
-        navigate("/home");
         window.location.reload();
       }
-    })
+    });
   };
 
-  return (
-    <div>
-      <Popup openPopup={props.openPopup} setOpenPopup={props.setOpenPopup}>
-        <form>
-          <Grid container>
-            <Grid item xs={12}>
-              <div className={styles.title}>
-                <Typography className={styles.editBillLabel}>Edit Bill</Typography>
-              </div>
-              <CustomTextField
-                className={styles.nameField}
-                defaultValue={props.billView.billName}
-                label="Name of Bill"
-                name="name"
-                variant="outlined"
-                type="text"
-                onChange={(e) => setName(e.target.value)}
-              />
-              <CustomTextField
-                className={styles.amountField}
-                defaultValue={props.billView.amount}
-                type="text"
-                label="Amount"
-                name="amount"
-                variant="outlined"
-                onChange={(e) => setAmount(e.target.value)}
-              />
-              <CustomTextField
-                className={styles.dateField}
-                onFocus={onFocus}
-                onBlur={onBlur}
-                defaultValue={props.billView.dueDate}
-                name="date"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                variant="outlined"
-                onChange={(e) => {
-                  if (e.target.value) {
-                    setDate(e.target.value);
-                    setHasValue(true);
-                  } else {
-                    setHasValue(false);
-                    setDate("");
-                  }
-                }}
-                label="Due Date"
-                type={hasValue || focus ? "date" : "text"}
-              />
-              <CustomButton className={styles.saveButton} variant="contained" onClick={handleEditBill}>
-                Save
-              </CustomButton>
+  render() {
+    return (
+      <div className={styles.popup}>
+        <Popup openPopup={this.props.openPopup} setOpenPopup={this.props.setOpenPopup}>
+          <form>
+            <Grid container>
+              <Grid item xs={12}>
+                <div className={styles.title}>
+                  <Typography className={styles.addBillLabel}>Bills</Typography>
+                </div>
+                <CustomTextField
+                  className={styles.nameField}
+                  value={this.state.billName}
+                  label="Name of Bill"
+                  name="name"
+                  variant="outlines"
+                  onChange={(e) => this.billNameHandler(e)}
+                />
+                <CustomTextField
+                  className={styles.amountField}
+                  value={this.state.amount}
+                  label="Amount"
+                  name="amount"
+                  variant="outlined"
+                  type="text"
+                  onChange={(e) => this.amountHandler(e)}
+                />
+                <CustomTextField
+                  className={styles.dateField}
+                  onFocus={this.state.onFocus}
+                  onBlur={this.state.onBlur}
+                  value={this.state.dueDate}
+                  name="date"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                  onChange={(e) => {
+                    if (e.target.value) {
+                      this.dueDateHandler(e);
+                      this.setHasValue(true);
+                    } else {
+                      this.setHasValue(false);
+                      this.dueDateHandler("");
+                    }
+                  }}
+                  label="Due Date"
+                  type={this.state.hasValue || this.state.focus ? "date" : "text"}
+                />
+                <CustomButton className={styles.saveButton} variant="contained" onClick={this.handleEditBill}>
+                  Save
+                </CustomButton>
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </Popup>
-    </div>
-  )
+          </form>
+        </Popup>
+      </div>
+    );
+  }
 }
